7e3fccceee753a2de961c1c571ba4fabe0ac82d8
khiemdev <khiembi161219@gmail.com>
2024-05-22 22:40:36 +0700

    Compiling TypeScript

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var charater = "hihihi";
+console.log(charater);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,9 @@
+const charater = "hihihi";
+console.log(charater);
+const inputs = document.querySelectorAll("input");
+
+console.log(inputs);
+
+inputs.forEach((input) => {
+  console.log(input);
+});
