65a8a9b93f72631702cd9fae98ba85fd11d3045d
khiemdev <khiembi161219@gmail.com>
2024-05-22 22:45:29 +0700

    Type Basics

--- sandbox.js
+++ sandbox.js
@@ -5,3 +5,12 @@ console.log(inputs);
 inputs.forEach(function (input) {
     console.log(input);
 });
+// Type Basics
+var stringDemo = "hi";
+var numberDemo = 10;
+var booleanDemo = true;
+stringDemo = 'hello';
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7));
--- sandbox.ts
+++ sandbox.ts
@@ -7,3 +7,16 @@ console.log(inputs);
 inputs.forEach((input) => {
   console.log(input);
 });
+
+
+// Type Basics
+let stringDemo = "hi";
+let numberDemo = 10;
+let booleanDemo = true;
+
+stringDemo = 'hello';
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+}
+console.log(circ(7));
