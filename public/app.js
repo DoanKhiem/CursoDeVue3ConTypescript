ef3e7cbf5f3cc90f166f3b019e265db4f615914d
khiemdev <khiembi161219@gmail.com>
2024-05-26 15:37:39 +0700

    Generics

--- public/app.js
+++ public/app.js
@@ -65,3 +65,21 @@ const greetPerson = (person) => {
     console.log('hello', person.name);
 };
 greetPerson(me);
+// Generics
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'yoshi', age: 40 });
+console.log(docOne.name);
+const docThree = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'shaun' }
+};
+const docFour = {
+    uid: 2,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk']
+};
+console.log(docThree, docFour);
