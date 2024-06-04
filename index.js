4ca6fc8fdb121f6de6707da829b94eb2a415c9bf
khiemdev <khiembi161219@gmail.com>
2024-06-04 22:02:53 +0700

    Type Basics

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 var age = 30;
-console.log(age);
+var firstName = 'John';
+var isFictional = true;
+// age = 'hello' // Error: Type 'string' is not assignable to type 'number'.
+age = 25; // OK
+firstName = 'Jane'; // OK
+isFictional = false; // OK
+var planet = 'Earth'; // Type is inferred as string
+var moons = 1; // Type is inferred as number
+var isLarge = false; // Type is inferred as boolean
+planet = 'Mars';
+moons = 145;
+isLarge = true;
+// null & undefined
+var something;
+var anotherThing;
+something = null;
+anotherThing = undefined;
--- index.ts
+++ index.ts
@@ -1,4 +1,25 @@
-let age = 30;
+let age: number = 30;
+let firstName: string = 'John';
+let isFictional: boolean = true;
+
+// age = 'hello' // Error: Type 'string' is not assignable to type 'number'.
+age = 25; // OK
+firstName = 'Jane'; // OK
+isFictional = false; // OK
+
+let planet = 'Earth'; // Type is inferred as string
+let moons = 1; // Type is inferred as number
+let isLarge = false; // Type is inferred as boolean
 
 
-console.log(age);
+planet = 'Mars'
+moons = 145
+isLarge = true
+
+// null & undefined
+let something: null
+let anotherThing: undefined
+
+something = null
+anotherThing = undefined
+
