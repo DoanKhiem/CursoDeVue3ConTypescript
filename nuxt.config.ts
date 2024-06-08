c913219e8d861296faadabd3fb447d12ae28c2b8
khiemdev <khiembi161219@gmail.com>
2024-06-08 10:13:10 +0700

    callout and file md

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', "@nuxt/content"],
   content: {
-    // ... options
+    highlight: {
+      theme: 'nord',
+      preload: ['ts', 'js', 'css', 'java', 'json', 'bash', 'vue']
+    }
   }
 })
