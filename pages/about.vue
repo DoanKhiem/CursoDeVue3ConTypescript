42787d82271b0d7a45cc5534ae60c8fc826010b3
khiemdev <khiembi161219@gmail.com>
2024-06-09 20:27:37 +0700

    nuxtlink

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
+        <header>
+            <nav>
+                <nuxt-link to="/">Đoàn Khiêm</nuxt-link>
+                <ul>
+                    <li><nuxt-link to="/">Home</nuxt-link></li>
+                    <li><nuxt-link to="/about">About</nuxt-link></li>
+                    <li><nuxt-link to="/products">Products</nuxt-link></li>
+                </ul>
+                <a href="/about">normal link  -- about page</a>
+            </nav>
+        </header>
         <h2>About</h2>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat minus odio nihil repellendus dolorum maxime explicabo consequatur numquam aperiam, beatae iure distinctio consectetur, ipsam doloribus eos, accusantium quaerat odit. Iste?</p>
     </div>
