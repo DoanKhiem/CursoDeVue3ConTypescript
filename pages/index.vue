83b3f37e4ac516868f33450c650e4addaff7c1d2
khiemdev <khiembi161219@gmail.com>
2024-06-09 20:19:21 +0700

    Adding Pages

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat minus odio nihil repellendus dolorum maxime explicabo consequatur numquam aperiam, beatae iure distinctio consectetur, ipsam doloribus eos, accusantium quaerat odit. Iste?</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
