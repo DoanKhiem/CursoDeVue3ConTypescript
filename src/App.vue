5fae5468714dadf3a6efa74fe2a3b9b99b249dbd
khiemdev <khiembi161219@gmail.com>
2024-06-12 14:25:10 +0700

    Resetting State & storeToRefs

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <!-- heading -->
     <header>
       <img src="./assets/pinia-logo.svg" alt="pinia logo">
-      <h1>{{ taskStore.name }}</h1>
+      <h1>{{ name }}</h1>
     </header>
 
     <!-- new task form -->
@@ -19,23 +19,25 @@
     </nav>
 
     <!-- loading -->
-    <div class="loading" v-if="taskStore.loading">Loading tasks...</div>
+    <div class="loading" v-if="loading">Loading tasks...</div>
 
     <!-- task list -->
     <div class="task-list" v-if="filter === 'all'">
-      <p>Your have {{ taskStore.totalCount }} tasks left to do</p>
-      <div v-for="task in taskStore.tasks" :key="task.id">
+      <p>Your have {{ totalCount }} tasks left to do</p>
+      <div v-for="task in tasks" :key="task.id">
         <!-- <p>{{ task.title }}</p> -->
         <TaskDetails :task="task" />
       </div>
     </div>
     <div class="task-list" v-if="filter === 'favs'">
-      <p>Your have {{ taskStore.favCount }} favs left to do</p>
-      <div v-for="task in taskStore.favs" :key="task.id">
+      <p>Your have {{ favCount }} favs left to do</p>
+      <div v-for="task in favs" :key="task.id">
         <!-- <p>{{ task.title }}</p> -->
         <TaskDetails :task="task" />
       </div>
     </div>
+
+    <button @click="taskStore.$reset">reset the state</button>
   </main>
 </template>
 
@@ -44,7 +46,10 @@ import { ref } from 'vue'
 import TaskDetails from './components/TaskDetails.vue'
 import TaskForm from './components/TaskForm.vue'
 import { useTaskStore } from './stores/TaskStore'
+import { storeToRefs } from 'pinia'
+
 const taskStore = useTaskStore()
+const { tasks, loading, favs, totalCount, favCount, name } = storeToRefs(taskStore)
 const filter = ref('all')
 // fetch tasks
 taskStore.getTasks()
