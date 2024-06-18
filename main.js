f12dc9edb1df41ffa0e4ad91688d4fe7b0880b84
doankhiem <khiembi161219@gmail.com>
2024-06-18 22:37:00 +0700

    Execution Context

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-function first() {
-  console.log('first....')
-  second();
+// function first() {
+//   console.log('first....')
+//   second();
+// }
+
+// function second() {
+//   console.log('second....')
+//   third();
+// }
+
+// function third() {
+//   console.log('third....')
+// }
+
+// first();
+
+var x = 100;
+var y = 50;
+
+console.log(getSum(x, y));
+
+function getSum(n1, n2) {
+  return n1 + n2;
 }
 
-function second() {
-  console.log('second....')
-  third();
-}
+var sum1 = getSum(x, y);
+var sum2 = getSum(10, 20);
 
-function third() {
-  console.log('third....')
-}
-
-first();
